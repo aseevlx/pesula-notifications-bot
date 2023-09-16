04a2ff9712ffcd06a4c6a144a9664929cc73f8ab
Alexander Aseev <alexander@aseev.im>
2023-09-17 00:35:58 +0300

    Add code, Dockerfile, readme

---
+++ README.md
@@ -0,0 +1,33 @@
+# Pesula / Laundry notifications bot
+
+This is a script that uses Telegram bot for notify you when your laundry is done.  
+It is intended to be used with the [Nortec](https://vuoronvaraus.fi) laundry reservation system.
+
+## Pre-requisites
+- Setup sending notifications to your email. Gmail can't be used because it doesn't support IMAP with only password auth
+- Get your IMAP server address and port
+- Get your email and password from your email provider
+- Create a tg bot with [BotFather](https://t.me/botfather) and get the bot token.
+- Add the bot to your group chat (or use your own id).
+
+# Usage
+- Build the image:
+```shell
+docker build -t pesula-bot .
+```
+
+- Run:
+```shell
+docker run -it --restart unless-stopped --name pesula-bot -d \
+    -e EMAIL_USERNAME=<your email> \
+    -e EMAIL_PASSWORD=<your password> \
+    -e IMAP_SERVER=<your imap server> \
+    -e TG_BOT_TOKEN=<your bot token> \
+    -e TG_CHAT_ID=<your chat id> \
+    -e TG_ERROR_CHAT_ID=<your error chat id> \
+    pesula-bot
+```
+
+
+## Possible improvements
+- Add notification when laundry shift is starting
---
+++ main.py
@@ -0,0 +1,130 @@
+import imaplib
+import email
+from email.header import decode_header
+import requests
+from time import sleep
+from typing import Optional
+from datetime import datetime
+import os
+
+EMAIL_USERNAME = os.getenv("EMAIL_USERNAME")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+IMAP_SERVER = os.getenv("IMAP_SERVER")
+TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
+TG_CHAT_ID = os.getenv("TG_CHAT_ID")
+TG_ERROR_CHAT_ID = os.getenv("TG_ERROR_CHAT_ID")
+
+TG_BASE_URL = f"https://api.telegram.org/bot{TG_BOT_TOKEN}"
+
+
+def get_new_emails(connection: imaplib.IMAP4_SSL) -> Optional[list]:
+    """
+    Fetching unread messages from INBOX
+    """
+    connection.select("INBOX")
+    status, messages = connection.search(None, '(UNSEEN)')
+
+    if status != 'OK':
+        raise ConnectionError("Error fetching new messages")
+
+    if messages == [b'']:
+        return None
+
+    message_numbers = str(messages[0].decode()).split(' ')[::-1]
+    return message_numbers
+
+
+def search_for_pesula_messages(connection: imaplib.IMAP4_SSL, message_numbers: list):
+    for message_number in message_numbers:
+        print(f'Processing: {message_number}')
+
+        res, msg = connection.fetch(str(message_number), "(RFC822)")
+        for response in msg:
+            if not isinstance(response, tuple):
+                continue
+
+            msg = email.message_from_bytes(response[1])
+            subject, encoding = decode_header(msg["Subject"])[0]
+            if isinstance(subject, bytes):
+                # if it's a bytes, decode to str
+                subject = subject.decode(encoding)
+
+            if "Færdig" not in subject:
+                continue
+
+            if not msg.is_multipart():
+                continue
+
+            for part in msg.walk():
+                content_type = part.get_content_type()
+                content_disposition = str(part.get("Content-Disposition"))
+
+                try:
+                    # get the email body
+                    body = part.get_payload(decode=True).decode()
+                except:
+                    continue
+
+                if content_type == "text/plain" and "attachment" not in content_disposition:
+                    if "Vaskemaskine" in body:
+                        start = body.find("Vaskemaskine")
+                        washing_machine = body[start:].split(",")[0]
+                        msg = f"Washing machine {washing_machine[-1]} is done"
+                        send_message_to_telegram(msg)
+
+                    if "Tørretumbler" in body:
+                        start = body.find("Tørretumbler")
+                        dryer = body[start:].split(",")[0]
+                        msg = f"Dryer {dryer[-1]} is done"
+                        send_message_to_telegram(msg)
+
+
+def send_message_to_telegram(message: str, error: bool = False):
+    payload = {'chat_id': TG_CHAT_ID, 'text': message}
+    if error:
+        payload['chat_id'] = TG_ERROR_CHAT_ID
+        payload['text'] = f"Pesula bot error: {message}"
+    requests.get(f"{TG_BASE_URL}/sendMessage", params=payload)
+
+
+def is_working_hours():
+    current_hour = int(datetime.now().strftime("%H"))
+    if 0 <= current_hour < 8:
+        return False
+    return True
+
+
+def main():
+    imap = imaplib.IMAP4_SSL(IMAP_SERVER)
+    imap.login(EMAIL_USERNAME, EMAIL_PASSWORD)
+
+    while True:
+        if not is_working_hours():
+            print("Right now the laundry room is not working, sleep for 30 minutes")
+            sleep(60 * 30)
+            continue
+
+        try:
+            messages_numbers = get_new_emails(imap)
+        except ConnectionError as e:
+            send_message_to_telegram(f"Error fetching new messages: {e}, sleep for 5 minutes", error=True)
+            sleep(60 * 5)
+            continue
+
+        if messages_numbers is None:
+            print("No new messages, sleep for 60 seconds")
+            sleep(60)
+            continue
+
+        search_for_pesula_messages(imap, messages_numbers)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted by user")
+        exit(0)
+    except Exception as e:
+        send_message_to_telegram(f"Error: {e}", error=True)
+        exit(1)
